90745a0138c5cb7d079d47a9dacef4a905741ed0
Sina Zadeh <s.hossinzade@gmail.com>
2025-07-27 13:14:32 -0500

    Initialize package.json with module type and dependencies

--- scraper.js
+++ scraper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as fs from "fs";
 import * as path from "path";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 
 const SITEMAPS = [
   "https://runrepeat.com/sitemaps/training-shoes/en-review-sitemap.xml",
