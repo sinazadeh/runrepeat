a2d07189775342c7600721839400d8d8f6326743
Sina Zadeh <s.hossinzade@gmail.com>
2025-07-28 00:24:05 -0500

    Bump version to 1.2.2 and update download/update URLs

--- RunRepeat_Review_Summaries_on_Shoe_Sites.user.js
+++ RunRepeat_Review_Summaries_on_Shoe_Sites.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RunRepeat Review Summaries on Shoe Sites
 // @namespace    https://github.com/sinazadeh/userscripts
-// @version      1.2.1
+// @version      1.2.2
 // @description  Injects RunRepeat reviews onto product pages of major shoe brands.
 // @author       TheSina
 // @match        https://www.nike.com/*
@@ -16,6 +16,8 @@
 // @grant        GM_xmlhttpRequest
 // @connect      runrepeat.com
 // @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/sinazadeh/runrepeat/refs/heads/main/RunRepeat_Review_Summaries_on_Shoe_Sites.user.js
+// @updateURL    https://raw.githubusercontent.com/sinazadeh/runrepeat/refs/heads/main/RunRepeat_Review_Summaries_on_Shoe_Sites.meta.js
 // ==/UserScript==
 /* jshint esversion: 11 */
 (function () {
