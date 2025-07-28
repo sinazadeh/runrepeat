702bdfe7cc9e5781d71565cdfa248426c6b9e321
sinazadeh <sinazadeh@users.noreply.github.com>
2025-07-28 05:21:20 +0000

    chore: update userscript meta

---
+++ RunRepeat_Review_Summaries_on_Shoe_Sites.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         RunRepeat Review Summaries on Shoe Sites
+// @namespace    https://github.com/sinazadeh/userscripts
+// @version      1.2.1
+// @description  Injects RunRepeat reviews onto product pages of major shoe brands.
+// @author       TheSina
+// @match        https://www.nike.com/*
+// @match        https://www.adidas.com/*
+// @match        https://www.newbalance.com/*
+// @match        https://www.asics.com/*
+// @match        https://www.brooksrunning.com/*
+// @match        https://www.hoka.com/*
+// @match        https://www.saucony.com/*
+// @match        https://www.altrarunning.com/*
+// @match        https://www.on.com/*
+// @grant        GM_xmlhttpRequest
+// @connect      runrepeat.com
+// @license      MIT
+// ==/UserScript==
